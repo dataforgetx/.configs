b0f3eb4b9a3c3d587b1e3a5df68fabcd6e0f25ec
Hanlong Fu <jack.hanlong@gmail.com>
2025-11-29 13:35:41 -0600

    changed colorscheme

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,17 +1,32 @@
 return {
-  "folke/tokyonight.nvim",
+  "catppuccin/nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require("tokyonight").setup({
-      style = "moon",
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
+    require("catppuccin").setup({
+      flavor = "macchiato",
+      transparent_background = true,
+      no_italic = true,
+      no_bold = true,
+      no_underline = true,
+      styles = {},
+      auto_integrations = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
       },
     })
 
     -- setup must be called before loading
-    vim.cmd.colorscheme("tokyonight")
+    vim.cmd.colorscheme("catppuccin")
+    vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", {
+      bg = "#45475a",
+      fg = "#cdd6f4",
+    })
+
+    vim.api.nvim_set_hl(0, "@markup.raw.block", {
+      bg = "#45475a",
+      fg = "#cdd6f4",
+    })
   end,
 }
